396b8a0dc9752c6f423ebef495dc85bc65565da2
Riccardo Giuliani <best-richard@hotmai.it>
2020-05-16 14:50:41 +0200

    Visualizzazione cover ottimizzata, aggiunta foto e sezioni vuote

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
+import { NavbarComponent as navmdb} from 'angular-bootstrap-md';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'emanuele-caloisi';
+  @ViewChild('navbarid')
+  private navbaridRef: navmdb;
+
+  onClick(): void {
+    if (this.navbaridRef.shown) {
+      this.navbaridRef.toggle();
+    }
+  }
 }
