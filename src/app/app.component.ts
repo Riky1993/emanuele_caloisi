7c6ed643f47ffacb57bbc4cf8cdda8c24ce3ff46
Riccardo Giuliani <best-richard@hotmai.it>
2020-05-17 12:11:37 +0200

    Creata classe per image in base64

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,23 @@
-import {Component, ViewChild} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import { NavbarComponent as navmdb} from 'angular-bootstrap-md';
+import {ImageInBase64} from '../assets/imageInBase64';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'emanuele-caloisi';
   @ViewChild('navbarid')
   private navbaridRef: navmdb;
+  public chiSono;
+
+  constructor(private base64Img: ImageInBase64) {}
+
+  ngOnInit() {
+    this.chiSono = this.base64Img.chiSono;
+  }
 
   onClick(): void {
     if (this.navbaridRef.shown) {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {MDBBootstrapModule} from 'angular-bootstrap-md';
+import {ImageInBase64} from '../assets/imageInBase64';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,7 @@ import {MDBBootstrapModule} from 'angular-bootstrap-md';
     AppRoutingModule,
     MDBBootstrapModule.forRoot()
   ],
-  providers: [],
+  providers: [ImageInBase64],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
