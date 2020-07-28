7f85fece3c1584dfd00ebe870b1010f26b0f75d8
Riccardo Giuliani <Riccardo.Giuliani@nttdata.com>
2020-07-28 17:05:11 +0200

    Cambiata navbar, cover image e testo centrale

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import {Component, OnInit, ViewChild} from '@angular/core';
-import { NavbarComponent as navmdb} from 'angular-bootstrap-md';
+import {AfterContentInit, Component, OnInit, ViewChild} from '@angular/core';
+import {NavbarComponent as navmdb} from 'angular-bootstrap-md';
 import {ImageInBase64} from '../assets/imageInBase64';
 
 @Component({
@@ -7,16 +7,24 @@ import {ImageInBase64} from '../assets/imageInBase64';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit, AfterContentInit {
   title = 'emanuele-caloisi';
   @ViewChild('navbarid')
   private navbaridRef: navmdb;
   public chiSono;
+  public ecLogo;
 
-  constructor(private base64Img: ImageInBase64) {}
+  constructor(private base64Img: ImageInBase64) {
+  }
+
+  ngAfterContentInit(): void {
+    const script = document.getElementsByTagName('nav')[0];
+    script.setAttribute('style', 'background: linear-gradient(to right, #FFFFFF, #EF3B36)');
+  }
 
   ngOnInit() {
     this.chiSono = this.base64Img.chiSono;
+    this.ecLogo = this.base64Img.getECLogo;
   }
 
   onClick(): void {
