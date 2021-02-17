79ddefc6c20e0e843eef4f8ce0384d7a0bc56840
Riccardo Giuliani <Riccardo.Giuliani@nttdata.com>
2021-02-17 22:07:53 +0100

    Prova per mobile

--- src/app/embed-social-widget/embed-social-widget.component.ts
+++ src/app/embed-social-widget/embed-social-widget.component.ts
@@ -1,4 +1,5 @@
 import {Component, Input, OnInit} from '@angular/core';
+import {DeviceDetectorService} from 'ngx-device-detector';
 
 @Component({
   selector: 'app-embed-social-widget',
@@ -7,8 +8,10 @@ import {Component, Input, OnInit} from '@angular/core';
 })
 export class EmbedSocialWidgetComponent implements OnInit {
   @Input() refId: string;
+  public isMobile: boolean;
 
-  constructor() {
+  constructor(public deviceService: DeviceDetectorService) {
+    this.isMobile = false;
   }
 
   ngOnInit(): void {
